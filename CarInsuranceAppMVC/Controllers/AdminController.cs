2cfc6ec6651c03da69ce71863f7a4ed1235233fa
elborg20 <mtgroble@gmail.com>
2018-12-03 16:29:14 -0700

    First commit

---
+++ CarInsuranceAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using CarInsuranceAppMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsuranceAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using(CarInsuranceEntities db = new CarInsuranceEntities())
+            {
+                var signups = db.SignUps;
+                var signUps = new List<SignUp>();
+                foreach (var signup in signups)
+                {
+                    var signUp = new SignUp();
+                    signUp.FirstName = signup.FirstName;
+                    signUp.LastName = signup.LastName;
+                    signUp.EmailAddress = signup.EmailAddress;
+                    signUp.Quote = signup.Quote;
+                    signUps.Add(signUp);
+                }
+                return View(signUps);
+            }
+        }
+    }
+}
---
+++ CarInsuranceAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarInsuranceAppMVC.Models;
+
+namespace CarInsuranceApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress, DateTime dateOfBirth, int carYear, string carMake, string carModel, string dUI, int speedingTickets, string fullCoverage)
+        {            
+            decimal quote = 50;
+
+            var signup = new SignUp();
+            using (CarInsuranceEntities db = new CarInsuranceEntities())
+            {
+                signup.FirstName = firstName;
+                signup.LastName = lastName;
+                signup.EmailAddress = emailAddress;
+                signup.DateOfBirth = dateOfBirth;
+                signup.CarYear = carYear;
+                signup.CarMake = carMake;
+                signup.CarModel = carModel;
+                signup.DUI = dUI;
+                signup.SpeedingTickets = speedingTickets;
+                signup.FullCoverage = fullCoverage;
+
+              
+                if ((DateTime.Now - dateOfBirth).Days < 6570) quote += 100;
+
+                else if ((DateTime.Now - dateOfBirth).Days < 9125 || (DateTime.Now - dateOfBirth).Days > 36500) quote += 25;
+
+                if (carYear < 2000 || carYear > 2015) quote += 25;
+
+                if (carMake.ToLower() == "porsche") quote += 25;
+
+                if (carModel == "911 Carrera" || carModel == "911 carrera" || carModel == "911Carrera" || carModel == "911carrera") quote += 25;
+
+                quote += (speedingTickets * 10);
+
+                if (dUI.ToLower() == "yes") quote = quote + (quote / 4);
+
+                if (fullCoverage.ToLower() == "yes") quote = quote + (quote / 2);
+
+                signup.Quote = Math.Round(quote , 2);
+                db.SignUps.Add(signup);
+                db.SaveChanges();
+            }
+
+            return Content("Your Quote total is: $" + signup.Quote + ".  Follow up with us for more information today!");
+        }
+    }
+}
